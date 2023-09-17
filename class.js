4d9c6a7214b5135714130980afdf39dfa8e17206
alaminislamrahat <alaminislamrahat@gmail.com>
2023-09-17 19:56:11 +0600

    new class

---
+++ class.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+    }
+
+}
+const student1 = new Student(45,"alamin");
+const student2 = new Student(54, "rahat");
+console.log(student1)
+console.log(student2)
