1b6836316f16a596555c4a6ffc73ada8c02722ff
alaminislamrahat <alaminislamrahat@gmail.com>
2023-09-17 14:07:39 +0600

    practice

---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = "justin";
+const lastName = "Jurvice";
+const fullName = firstName + " " + lastName + "is a great man";  //normal js
+
+const fullName2 = `${firstName} ${lastName} is a good guy`;  // es 6 js
+// console.log(fullName2);
+// console.log(fullName);
+
+const multiline = "I am rahat.\n" + "I am 20 years old.\n" + "I love coding"; //normal js
+const multiline2 = `I am AL-amin.
+I am 20 years old
+I love coding.`;    // es6 js
+console.log(multiline2);
+console.log(multiline);
